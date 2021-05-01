3fcefd2b2a452294a43b1522488fa66ce5f94d80
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-05-02 00:17:33 +0800

    add trivial settings

--- Learned.md
+++ Learned.md
@@ -4,3 +4,7 @@
 
 `java --module-path D:/ShiZu_Code/Java/javafx-sdk-15.0.1/lib --add-modules javafx.controls,javafx.fxml,javafx.media -jar wallpaper_master-1.0-SNAPSHOT.jar`
 `java --module-path JAVAFX_PATH --add-modules JAVAFX_JARS -jar JAVAFX_PROJECT.jar`
+
+## 注意引入新 Dependencies 時, 盯著 Git 看, 注意 target classes 是否被刪掉...
+
+[VSCode 硬核引入 .jar 的方法](https://dzone.com/articles/simple-ways-to-add-and-work-with-a-jar-file-in-you)
--- src/main/java/eroiko/ani/controller/ControllerSupporter/MinimizeWindow.java
+++ src/main/java/eroiko/ani/controller/ControllerSupporter/MinimizeWindow.java
@@ -1,13 +1,18 @@
 package eroiko.ani.controller.ControllerSupporter;
 
+import com.dustinredmond.fxtrayicon.FXTrayIcon;
+
+import eroiko.ani.MainApp;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class MinimizeWindow {
-    public static void minimizeWindow(Stage stage){
+    public void minimizeWindow(Stage stage){
         Platform.setImplicitExit(false);
         stage.setOnCloseRequest(event -> minimizeAStage());
         stage.addEventFilter(KeyEvent.KEY_PRESSED, (e) -> {
@@ -17,53 +22,11 @@ public class MinimizeWindow {
         });
     }
     
-    private static void minimizeAStage(){
-        // FXTrayIcon
+    private void minimizeAStage(){
         
-        //Check the SystemTray is supported
-        // if (!SystemTray.isSupported()) {
-        //     System.out.println("SystemTray is not supported");
-        //     return;
-        // }
-        // final PopupMenu popup = new PopupMenu();
-
-        // URL url = System.class.getResource("/images/new.png");
-        // Image image = Toolkit.getDefaultToolkit().getImage(url);
-
-        // final TrayIcon trayIcon = new TrayIcon(image);
-
-        // final SystemTray tray = SystemTray.getSystemTray();
-
-        // // Create a pop-up menu components
-        // MenuItem aboutItem = new MenuItem("About");
-        // CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        // CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        // Menu displayMenu = new Menu("Display");
-        // MenuItem errorItem = new MenuItem("Error");
-        // MenuItem warningItem = new MenuItem("Warning");
-        // MenuItem infoItem = new MenuItem("Info");
-        // MenuItem noneItem = new MenuItem("None");
-        // MenuItem exitItem = new MenuItem("Exit");
-
-        // //Add components to pop-up menu
-        // popup.add(aboutItem);
-        // popup.addSeparator();
-        // popup.add(cb1);
-        // popup.add(cb2);
-        // popup.addSeparator();
-        // popup.add(displayMenu);
-        // displayMenu.add(errorItem);
-        // displayMenu.add(warningItem);
-        // displayMenu.add(infoItem);
-        // displayMenu.add(noneItem);
-        // popup.add(exitItem);
-
-        // trayIcon.setPopupMenu(popup);
-
-        // try {
-        //     tray.add(trayIcon);
-        // } catch (AWTException e) {
-        //     System.out.println("TrayIcon could not be added.");
-        // }
+        // FXTrayIcon trayIcon = new FXTrayIcon(MainApp.mainStage, getClass().getClassLoader().getResource("eroiko.ain.img.wallpaper79.ico"));
+        // MenuItem menu = new MenuItem("Meow!");
+        // menu.setOnAction(e -> new Alert(Alert.AlertType.INFORMATION, "Clicked on Menu!").showAndWait());
+        // trayIcon.addMenuItem(menu);
     }
 }
